8b677ede7dc9dd997047a0420effc8b9c2498a33
mgdj <1754314623@qq.com>
2021-07-16 21:40:01 +0800

    homework5

---
+++ homework5/app/src/main/java/com/bytedance/practice5/Constants.java
@@ -0,0 +1,12 @@
+package com.bytedance.practice5;
+
+public class Constants {
+    public static final String BASE_URL = "https://api-android-camp.bytedance.com/zju/invoke/";
+    public static final String token = "U0pUVS1ieXRlZGFuY2UtYW5kcm9pZA==";
+    //TODO 1这里写上自己的学号和名字
+    public static final String STUDENT_ID = "3190104791";
+    public static final String USER_NAME = "阮奕捷";
+    public static final String BOUNDARY = "156383456";
+    public static final String PRE_FIX = ("\r\n--" + BOUNDARY + "\r\n");
+    public static final String END_FIX = ("\r\n--" + BOUNDARY + "--\r\n");
+}
